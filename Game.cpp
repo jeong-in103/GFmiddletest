2feb7dece73c92c71626b09fb15ba77642603e8a
20201312 문정인 <83389445+jeong-in103@users.noreply.github.com>
2021-10-29 03:05:06 +0000

    유일객체 Game

--- Game.cpp
+++ Game.cpp
@@ -2,6 +2,8 @@
 #include "TextureManager.h"
 #include "Player.h"
 
+Game* Game::s_pInstance = 0;
+
 bool Game::init(const char *title, int xpos, int ypos, int width, int height, int flags)
 {
   if (SDL_Init(SDL_INIT_EVERYTHING) == 0) 
--- Game.h
+++ Game.h
@@ -7,7 +7,20 @@
 class Game 
 {
 public:
-  Game() {}
+  static Game* Instance()
+  {
+    if(s_pInstance == 0)
+    {
+      s_pInstance = new Game();
+      return s_pInstance;
+    }
+    return s_pInstance;
+  }
+  SDL_Renderer* getRenderer() const 
+  {
+    return m_pRenderer;
+  }
+
   ~Game() {}
   bool init(const char *title, int xpos, int ypos, int width, int height, int flags);
   void render();
@@ -16,6 +29,9 @@ public:
   void handleEvents();
   void clean();
 private:
+  Game() {}
+  static Game* s_pInstance;
+
   SDL_Window* m_pWindow;
   SDL_Renderer* m_pRenderer;
 
@@ -23,5 +39,7 @@ private:
   
   bool m_bRunning;
 };
+
+typedef Game TheGame;
 
 #endif
