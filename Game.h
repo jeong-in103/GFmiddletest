ce846a4304a8c8e35337df0c7e9c6d64d66e9f70
20201312 문정인 <83389445+jeong-in103@users.noreply.github.com>
2021-10-29 02:07:37 +0000

    3

--- Game.h
+++ Game.h
@@ -17,6 +17,11 @@ public:
 private:
   SDL_Window* m_pWindow;
   SDL_Renderer* m_pRenderer;
+
+  SDL_Texture* m_pTexture;
+  SDL_Rect m_sourceRectangle;
+  SDL_Rect m_destinationRectangle;
+
   bool m_bRunning;
 };
 
